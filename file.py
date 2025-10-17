8acf5d1de0499aaca4f7802f5df63aad79de6a47
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-17 21:20:24 +0530

    fixed the code

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 class_held = int(input("Enter number of classes held: "))
 class_attended = int(input("Enter number of classes attended: "))
-percentage_attendence = ((class_held/class_attended) * 100)
+percentage_attendence = ((class_attended/class_held) * 100)
 print(f"Classes Held = {class_held} \nClasses attended = {class_attended}\n Percentage of classes attended = {percentage_attendence}%")
 
 if (percentage_attendence >= 75):
