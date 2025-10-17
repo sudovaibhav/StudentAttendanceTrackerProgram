c779e6fc4149febaca439cb2fef9da6327d6a4f9
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-17 21:15:58 +0530

    Added eligibility check

--- file.py
+++ file.py
@@ -1,4 +1,9 @@
-class_held = int(input("Enter number of classes held: ")
-class_attended = int(input("Enter number of classes attended: ")
+class_held = int(input("Enter number of classes held: "))
+class_attended = int(input("Enter number of classes attended: "))
 percentage_attendence = ((class_held/class_attended) * 100)
 print(f"Classes Held = {class_held} \nClasses attended = {class_attended}\n Percentage of classes attended = {percentage_attendence}%")
+
+if (percentage_attendence >= 75):
+    print("Student is eligible to write exam.")
+else: 
+    print("Student is not eligible to write exam.")
