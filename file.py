a2fe393f1efa68f1c5c2e30cdd11e9ed1658449f
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-17 21:07:19 +0530

    Add attendance percentage calculation

---
+++ file.py
@@ -0,0 +1,4 @@
+class_held = int(input("Enter number of classes held: ")
+class_attended = int(input("Enter number of classes attended: ")
+percentage_attendence = ((class_held/class_attended) * 100)
+print(f"Classes Held = {class_held} \nClasses attended = {class_attended}\n Percentage of classes attended = {percentage_attendence}%")
